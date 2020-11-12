a81c819b83b5c908c1ae7fb2090b75e52bcf143e
Dyomin Ilya <alkhipce@mail.ru>
2020-11-13 02:12:27 +0600

    feat: new build

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -9,7 +9,6 @@ import {
 } from './utils'
 
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\node_modules\\bulma\\css\\bulma.css'
 
@@ -57,7 +56,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -182,10 +181,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
---
+++ .nuxt/dist/server/f2e1cf4c2ebf52ae6f37.js
@@ -0,0 +1,1 @@
+exports.ids=[5],exports.modules={173:function(t,e,n){"use strict";n.r(e);var r={},c=n(1);var component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("main",{staticClass:"main"},[])}),[],!1,(function(t){}),null,"104b75ac");e.default=component.exports}};
