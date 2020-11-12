5a817a083cda4ad979b55a72e452c035ff7d41ea
Dyomin Ilya <alkhipce@mail.ru>
2020-11-13 00:00:56 +0600

    add build

---
+++ .nuxt/dist/server/53dc94a23dd3fd5fb3cc.js
@@ -0,0 +1,1 @@
+exports.ids=[11],exports.modules={171:function(t,e,n){"use strict";n.r(e);var r={},c=n(1);var component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("main",{staticClass:"main"},[])}),[],!1,(function(t){}),null,"efdfd042");e.default=component.exports}};
