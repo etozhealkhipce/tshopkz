f12651172af60f0d7efcffcd0d3968dfba1c9408
Dyomin Ilya <alkhipce@mail.ru>
2020-11-23 18:14:39 +0600

    fix: search fix;

--- .nuxt/dist/server/7d2b80db0d0e882d3ae2.js
+++
@@ -1,1 +0,0 @@
-exports.ids=[11],exports.modules={174:function(t,e,n){"use strict";n.r(e);var r={},c=n(1);var component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("main",{staticClass:"main"},[])}),[],!1,(function(t){}),null,"efdfd042");e.default=component.exports}};
---
+++ store/results.page.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  results: []
+})
+
+export const mutations = {
+  ADD_RESULTS(state, results) {
+    state.results = results
+  }
+}
+
+export const actions = {
+  async fetchResults({ commit }, search) {
+    const response = await this.$axios.post('product/search', {
+      title: search
+    })
+
+    commit('ADD_RESULTS', response.data)
+    console.log(response)
+  }
+}
+
+export const getters = {}
