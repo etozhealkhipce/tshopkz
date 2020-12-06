8eda995109b4b451485fc78812db17f44129de53
Dyomin Ilya <alkhipce@mail.ru>
2020-12-07 00:10:57 +0600

    chore: add cluster mode for pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'tshop-prod',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
       script: 'npm',
       args: 'run start'
     }
