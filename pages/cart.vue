679416e065690bd2a235cda22669f67cfd704ff6
Dyomin Ilya <alkhipce@mail.ru>
2020-12-11 19:54:18 +0600

    feat: add sdek api

--- pages/cart.vue
+++ pages/cart.vue
@@ -662,10 +662,38 @@ export default {
     async fetchWishlist() {
       await this.$store.dispatch('account/fetchWishlist')
     },
-    setReceiverCity(currentReceiverCity) {
+    async setReceiverCity(currentReceiverCity) {
       this.order.receiverCity = currentReceiverCity.name
       this.order.receiverCityId = currentReceiverCity.code
       this.receiverCityPostCode = currentReceiverCity.postCode
+
+      const goods = []
+      this.productsArr.forEach((product) => {
+        const good = {
+          weigth: product.weight,
+          lenght: product.length,
+          width: product.width,
+          height: product.height
+        }
+
+        goods.push(good)
+      })
+
+      const body = {
+        goods,
+        senderCityId: this.order.senderCityId,
+        receiverCityId: this.order.receiverCityId,
+        tariffList: this.order.tariffList
+        // services: [
+        //   {
+        //     id: 2,
+        //     param: 1000
+        //   }
+        // ]
+      }
+
+      const response = await this.$axios.post('/sdet-api-for-calc-price', body)
+      console.log(response)
     },
     async checkPromo() {
       try {
